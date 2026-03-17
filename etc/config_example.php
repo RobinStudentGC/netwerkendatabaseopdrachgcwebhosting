1b1b0cd57db8d380caa43c6f96a2caea8411ebe4
Robin Peter Miljoen <robin.miljoen@student.graafschapcollege.nl>
2026-03-17 09:15:27 +0100

    Changes to be committed: 	modified:   etc/config_example.php

--- etc/config_example.php
+++ etc/config_example.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL server.
 Please change this file to config.php and change these defines: */
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'db_user');
-define('DB_PASSWORD', 'db_password');
-define('DB_NAME', 'db_name');
+define('DB_USERNAME', 'rmiljoen_db_php_oop');
+define('DB_PASSWORD', 'kvEcK592MvbLHeLtZBTY');
+define('DB_NAME', 'rmiljoen_database');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
